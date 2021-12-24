fde0972a641a2d411590214b005f7d32c21e6618
Rafael <rafaelangelowow@gmail.com>
2021-12-24 17:58:50 -0300

    alter

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -3,9 +3,6 @@ import {
   GoogleMap,
   Marker,
   LoadScript,
-  StandaloneSearchBox,
-  DirectionsService,
-  DirectionsRenderer,
 } from "@react-google-maps/api";
 import { REACT_APP_GOOGLE_API_KEY } from "../App";
 import "./MapPage.css";
@@ -15,103 +12,16 @@ export interface MapPageProps {}
 
 const MapPage = () => {
   const [map, setMap] = React.useState<google.maps.Map>();
-  const [searchBoxA, setSearchBoxA] =
-    React.useState<google.maps.places.SearchBox>();
-  const [searchBoxB, setSearchBoxB] =
-    React.useState<google.maps.places.SearchBox>();
-  const [pointA, setPointA] = React.useState<google.maps.LatLngLiteral>();
-  const [pointB, setPointB] = React.useState<google.maps.LatLngLiteral>();
 
-  const [origin, setOrigin] = React.useState<google.maps.LatLngLiteral | null>(
-    null
-  );
-  const [destination, setDestination] =
-    React.useState<google.maps.LatLngLiteral | null>(null);
-
-  const [response, setResponse] =
-    React.useState<google.maps.DistanceMatrixResponse | null>(null);
-
-    const [position, setPosition] =
-    React.useState<google.maps.LatLngLiteral | google.maps.LatLng >({
-      lat: IMOVEIS[0].lat,
-      lng: IMOVEIS[0].lng,
-    });
+  const [apresentarNome, setApresentarNome] = React.useState<boolean>(false);
 
   const onMapLoad = (map: google.maps.Map) => {
     setMap(map);
   };
 
-  const onLoadA = (ref: google.maps.places.SearchBox) => {
-    setSearchBoxA(ref);
-  };
-
-  const onLoadB = (ref: google.maps.places.SearchBox) => {
-    setSearchBoxB(ref);
-  };
-
-  const onPlacesChangedA = () => {
-    const places = searchBoxA!.getPlaces();
-    console.log(places);
-    const place = places![0];
-    const location = {
-      lat: place?.geometry?.location?.lat() || 0,
-      lng: place?.geometry?.location?.lng() || 0,
-    };
-    setPointA(location);
-    setOrigin(null);
-    setDestination(null);
-    setResponse(null);
-    map?.panTo(location);
-  };
-
-  const onPlacesChangedB = () => {
-    const places = searchBoxB!.getPlaces();
-    console.log(places);
-    const place = places![0];
-    const location = {
-      lat: place?.geometry?.location?.lat() || 0,
-      lng: place?.geometry?.location?.lng() || 0,
-    };
-    setPointB(location);
-    setOrigin(null);
-    setDestination(null);
-    setResponse(null);
-    map?.panTo(location);
-  };
-
-  const traceRoute = () => {
-    if (pointA && pointB) {
-      setOrigin(pointA);
-      setDestination(pointB);
-    }
-  };
-
-  const directionsServiceOptions =
-    // @ts-ignore
-    React.useMemo<google.maps.DirectionsRequest>(() => {
-      return {
-        origin,
-        destination,
-        travelMode: "DRIVING",
-      };
-    }, [origin, destination]);
-
-  const directionsCallback = React.useCallback((res) => {
-    if (res !== null && res.status === "OK") {
-      setResponse(res);
-    } else {
-      console.log(res);
-    }
-  }, []);
-
-  const directionsRendererOptions = React.useMemo<any>(() => {
-    return {
-      directions: response,
-    };
-  }, [response]);
-
   return (
     <div className="map">
+    <button onClick={() => setApresentarNome(!apresentarNome)}>Habilitar e desabilitar nome</button>
       <LoadScript
         googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}
         libraries={["places"]}
@@ -119,46 +29,15 @@ const MapPage = () => {
         <GoogleMap
           onLoad={onMapLoad}
           mapContainerStyle={{ width: "100%", height: "100%" }}
-          center={position}
+          center={{
+            lat: IMOVEIS[0].lat,
+            lng: IMOVEIS[0].lng,
+          }}
           zoom={6}
         >
-          <div className="address">
-            <StandaloneSearchBox
-              onLoad={onLoadA}
-              onPlacesChanged={onPlacesChangedA}
-            >
-              <input
-                className="addressField"
-                placeholder="Digite o endereço inicial"
-              />
-            </StandaloneSearchBox>
-            <StandaloneSearchBox
-              onLoad={onLoadB}
-              onPlacesChanged={onPlacesChangedB}
-            >
-              <input
-                className="addressField"
-                placeholder="Digite o endereço final"
-              />
-            </StandaloneSearchBox>
-            <button onClick={traceRoute}>Traçar rota</button>
-          </div>
           {IMOVEIS.map((imovel) =>
-             <Marker key={imovel.id} position={{lat: imovel.lat, lng: imovel.lng}} label={imovel.nomeLoja} />
-        )}
-          {!response && pointA && <Marker position={pointA} />}
-          {!response && pointB && <Marker position={pointB} />}
-
-          {origin && destination && (
-            <DirectionsService
-              options={directionsServiceOptions}
-              callback={directionsCallback}
-            />
-          )}
-
-          {response && directionsRendererOptions && (
-            <DirectionsRenderer options={directionsRendererOptions} />
-          )}
+             <Marker key={imovel.id} position={{lat: imovel.lat, lng: imovel.lng}} label={apresentarNome ? imovel.nomeLoja : ''} />
+        )},
         </GoogleMap>
       </LoadScript>
     </div>
